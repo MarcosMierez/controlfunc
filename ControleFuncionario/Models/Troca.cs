1b62c3db0a9378973e60121c9c24671f1ad84362
Marcos Mierez <ka-ladze@hotmail.com>
2019-03-28 11:49:57 -0400

    primeiro commit

---
+++ ControleFuncionario/Models/Troca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFuncionario.Models
+{
+    public class Troca
+    {
+        public string IdTroca { get; set; }
+        public int IdSoliciatante { get; set; }
+        public int IdSolicitado { get; set; }
+        public string StatusTroca { get; set; } // solicitada - aprovada - recusada
+    }
+}
---
+++ ControleFuncionario/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFuncionario.Models
+{
+    public class Usuario
+    {
+        public string NomeUsuario { get; set; }
+        public string FuncaoUsuario { get; set; } // tecnico/administrador
+        public string EscalaUsuario { get; set; } // dia / noite
+    }
+}
